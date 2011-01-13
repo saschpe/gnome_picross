9ca95a4fd4c624becb6e3b3afef0bb3493bafae5
TvrtkoM <tvrtkom@gmail.com>
2011-01-13 20:46:25 +0100

    PeriodicTimer - GameTimer. Thread manages all ingame time functionality.
    GameTimer construction now requires two more arguments: playTime
    and timePenalty
    threading.Event used for pausing the timer.
    new member functions (check code)

--- gnomepicross/timer.py
+++ gnomepicross/timer.py
@@ -17,12 +17,14 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #
 
-from threading import Thread
+from threading import Thread, Event
 from time import sleep
 
+DEFAULT_PLAYTIME = 1800
+DEFAULT_TIME_PENALTY = 120
+DEFAULT_TIME_PENALTY_CAP = 480
 
-
-class PeriodicTimer(Thread):
+class GameTimer(Thread):
 	"""A generic periodic timer.
 
 	Executes a callback every 'interval' times after 'delay' time.
@@ -36,19 +38,29 @@ class PeriodicTimer(Thread):
 	"""
 
 
-	def __init__(self,delay=0,interval=1,callback=None,args=[],kwargs={}):
+	def __init__(
+			self,
+			playTime=DEFAULT_PLAYTIME,
+			timePenalty=DEFAULT_TIME_PENALTY,
+			delay=0,
+			interval=1,
+			callback=None,
+			args=[],kwargs={}
+	):
 		"""Initializes the timer.
 
 		Parameters:
-		delay		- Starting time after which the timer beginns to
+		playTime	- Time to play (in seconds)
+		timePenalty	- Applied when the user opens a wrong field
+		delay		- Starting time after which the timer begins to
 					  periodically execute the callback ( >= 0)
-		interval	- Callback execution intervall ( >= 0)
+		interval	- Callback execution interval ( >= 0)
 		callback	- The function to be run by the timer
 		args,kwargs	- Arguments to the callback
 		"""
 		Thread.__init__(self)
-		if delay < 0 or interval < 0:
-			raise ValueError('Delay and interval must be greater than zero!')
+		if delay < 0 or interval < 0 or playTime < 0 or timePenalty < 0: # interval can be zero?
+			raise ValueError('delay, interval, playTime, and timePenalty must be greater than zero!')
 		self.__delay = delay
 		self.__interval = interval
 		self.__callback = callback
@@ -56,12 +68,24 @@ class PeriodicTimer(Thread):
 		self.__kwargs = kwargs
 		self.__finished = False
 		self.__paused = False
-		self.setDaemon(True)
+		self.__restart = False
+		self.__pause = Event()
+		self.__timeLeft = self.__playTime = playTime
+		self.__currentTimePenalty = self.__timePenalty = timePenalty
+
+	def _debug_print(self):
+		print 'time left: %s play time: %s' % self.getTimes()
+		for row in self.__level:
+			print row
 
 	def pause(self):
 		"""For suspending and resuming the timer.
 		"""
 		self.__paused = not self.__paused
+		if self.__paused:
+			self.__pause.clear()
+		else:
+			self.__pause.set()
 
 	def cancel(self):
 		"""Finish execution. 
@@ -70,12 +94,45 @@ class PeriodicTimer(Thread):
 		you want to use it again.
 		"""
 		self.__finished = True
-
+	
+	def applyPenalty(self):
+		""" Apply time penalty if non-valid field clicked on board
+		"""
+		self.__timeLeft -= self.__currentTimePenalty
+		self.__currentTimePenalty += self.__timePenalty
+		if self.__currentTimePenalty > DEFAULT_TIME_PENALTY_CAP:
+			self.__currentTimePenalty = DEFAULT_TIME_PENALTY_CAP
+		if self.__timeLeft < 0: 
+			self.__timeLeft = 0
+			
+	def getTimes(self):
+		"""Returns the time left and the overall time
+		"""
+		return self.__timeLeft, self.__playTime
+	
+	def setPlayTime(self, playTime):
+		self.__timeLeft = self.__playTime = playTime
+	
+	def isPaused(self):
+		return self.__paused
+			
+	def restart(self):
+		"""Restart the timer.
+		
+		Used for reseting the timer for new game
+		"""
+		self.__restart = True
+		if self.__paused:
+			self.__pause.set()
+		
 	def run(self):
 		sleep(self.__delay)
+		self.__pause.set()
 		while not self.__finished:
-			if not self.__paused:
-				if self.__callback:
-					self.__callback(*self.__args,**self.__kwargs)
-				sleep(self.__interval-1)
-			sleep(1)
+			if self.__restart:
+				self.__timeLeft = self.__playTime
+				self.__restart = False
+			self.__pause.wait()
+			self.__callback()
+			sleep(self.__interval)
+			self.__timeLeft -= self.__interval
